0b006a71160bc25ac8948ce404478f089732e0ea
Unknown <resa.chan97@gmail.com>
2017-02-26 12:29:12 -0500

    Final w/o strategy

--- src/main/java/cybercycles/AI.java
+++ src/main/java/cybercycles/AI.java
@@ -11,7 +11,7 @@ public class AI {
 
     /* Configuration */
     public final String ROOM = "3";
-    public final String TEAM = "2";
+    public final String TEAM = "3";
 
     /* Déplacement de l'A.I. */
     public final static char[] directions = {'u', 'l', 'd', 'r'};
@@ -30,6 +30,73 @@ public class AI {
      * @param d the direction
      * @return true if you can go in that direction
      */
+    
+//    int possibilities (char ch) //change isPossible method
+//    {
+//        int poss =0;
+//        boolean a = isPoss(p[me-1].getX()+1,p[me-1].getY()-1) ;
+//        boolean b = isPoss(p[me-1].getX()-1,p[me-1].getY()-1) ;
+//        boolean c = isPoss(p[me-1].getX(),p[me-1].getY()-2) ;
+//        boolean d = isPoss(p[me-1].getX(),p[me-1].getY()+2) ;
+//        boolean e = isPoss(p[me-1].getX()-2,p[me-1].getY()) ;
+//        boolean f = isPoss(p[me-1].getX()+1,p[me-1].getY()+1) ;
+//        boolean g = isPoss(p[me-1].getX()+2,p[me-1].getY()) ;
+//        boolean h = isPoss(p[me-1].getX()-1,p[me-1].getY()+1) ;
+//        
+//        switch (ch)
+//        {
+//            case 'u':
+//                if (a) {
+//                    poss++;
+//                }
+//                if (b) {
+//                    poss++;
+//                }
+//                if (c) {
+//                    poss++;
+//                }
+//                break;
+//            case 'd':
+//                if (f) {
+//                    poss++;
+//                }
+//                if (h) {
+//                    poss++;
+//                }
+//                if (d) {
+//                    poss++;
+//                }
+//                break;
+//                
+//            case 'r':
+//                if (a) {
+//                    poss++;
+//                }
+//                if (f) {
+//                    poss++;
+//                }
+//                if (g) {
+//                    poss++;
+//                }
+//                
+//                break;
+//                
+//            case 'l':
+//                if (b) {
+//                    poss++;
+//                }
+//                if (h) {
+//                    poss++;
+//                }
+//                if (e) {
+//                    poss++;
+//                }
+//                break;
+//        }
+//        return poss;
+//            
+//    }
+    
     public boolean isPossible(char d) {
         Player play = p[me-1];
         
@@ -52,6 +119,7 @@ public class AI {
                     }
                 return !isOccupied[play.getY()][play.getX() - 1];
                 case 'r':
+                    System.out.println(play);
                     if (play.getX() + 1 >=isOccupied[0].length) {
                         return false;
                     }
@@ -61,38 +129,145 @@ public class AI {
                 throw new AssertionError();
         }
     }
-    public int possibility(int y, int x) {
-        int poss = 0;
-        if (y - 1 >= 0) {
-
-            if (!isOccupied[y - 1][x]) {
-                poss++;
-
-            }
+//    public int possibility(int y, int x) {
+//        int poss = 0;
+//        if (y - 1 >= 0) {
+//
+//            if (!isOccupied[y - 1][x]) {
+//                poss++;
+//
+//            }
+//        }
+//
+//        if (y + 1 < isOccupied.length) {
+//
+//            if (!isOccupied[y + 1][x]) {
+//                poss++;
+//
+//            }
+//        }
+//        if (x - 1 >= 0) {
+//
+//            if (!isOccupied[y][x - 1]) {
+//                poss++;
+//
+//            }
+//        }
+//        if (x + 1 < isOccupied[0].length) {
+//
+//            if (!isOccupied[y][x + 1]) {
+//                poss++;
+//
+//            }
+//        }
+//        return poss;
+//    }
+    
+//    //meeee tam
+//    public int getNextY(char c)
+//    {
+//        Player play = p[me-1];
+//        int y = p[me-1].getY();
+//        switch (c) {
+//            case 'u':
+//                y= y-1;
+//                break;
+//            case 'd':
+//                y= y+1;
+//                break;
+//            case 'r':
+//                y=y;
+//                break;
+//            case 'l':
+//                y=y;
+//                break;
+//            default:
+//                break;
+//        }
+//        
+//        return y;
+//    }
+//    
+//    public int getNextX(char c)
+//    {
+//        Player play = p[me-1];
+//        int x=p[me-1].getX();
+//        switch (c) {
+//            case 'u':
+//                x=x;
+//                break;
+//            case 'd':
+//                x=x;
+//                break;
+//            case 'r':
+//                x= x+1;
+//                break;
+//            case 'l':
+//                x= x-1;
+//                break;
+//            default:
+//                break;
+//        }
+//        
+//        return x;
+//    }
+//    
+//    public int possiNum ()
+//    {
+//        int possiM = 0;
+//        int possiU = 0;
+//        int possiD = 0;
+//        int possiL = 0;
+//        int possiR = 0;
+//        for (int i = 0; i < 4; i++) {
+//            char c=this.directions[i];
+//        
+//        
+//        isPoss(getNextX(c),getNextY(c),c );
+//        if (!false)
+//        {
+//            if (c =='u') {
+//                  
+//            }
+//            else if (c =='d') {
+//                 ++possiD;
+//            }
+//            else if (c =='l') {
+//                 ++possiL;
+//            }
+//            else if (c =='r') {
+//                 ++possiR;
+//            }
+//        }
+//        possiM=possiU;
+//            if (possiM < possiD) {
+//                possiM=possiD;
+//            }
+//            else if (possiM< possiL)
+//            {
+//                possiM=possiL;
+//            }
+//            else if (possiM<possiR) {
+//                possiM=possiR;
+//            }
+//        }
+//        
+//        return possiM;
+//    }
+    
+    public boolean isPoss (int x, int y)//good maybe
+    {
+        
+        if (x >=0 && x<isOccupied[0].length && y>=0 && y<isOccupied.length) {
+            return !isOccupied[y][x];
         }
-
-        if (y + 1 < isOccupied.length) {
-
-            if (!isOccupied[y + 1][x]) {
-                poss++;
-
-            }
-        }
-        if (x - 1 >= 0) {
-
-            if (!isOccupied[y][x - 1]) {
-                poss++;
-
-            }
-        }
-        if (x + 1 < isOccupied[0].length) {
-
-            if (!isOccupied[y][x + 1]) {
-                poss++;
-
-            }
-        }
-        return poss;
+        
+        else {return false;}
+        
+        
+ 
+       
+        
     }
 
     
@@ -112,7 +287,7 @@ public class AI {
         int heigthGrill = config.getInt("h");
 
         for (int i = 0; i < a.length(); i++) {
-            p[i] = new Player(a.getJSONObject(i));
+            p[Integer.parseInt(a.getJSONObject(i).getString("id"))-1] = new Player(a.getJSONObject(i));
             System.out.println(p[i]);
         }
 
@@ -145,8 +320,8 @@ public class AI {
             }
             System.out.printf("x: %d w: %d y: %d h: %d\n", x, w, y, h);
 
-            for (int row = y; row < y + h; row++) {//
-                for (int col = x; col < x + w; col++) {
+            for (int row = y; row < Math.min(y + h, isOccupied.length); row++) {//
+                for (int col = x; col < Math.min(x + w, isOccupied[0].length); col++) {
                     isOccupied[row][col] = true;
                 }
             }
@@ -180,9 +355,10 @@ public class AI {
         // saves opponent's moves
         for (int i = 0; i < prevMoves.length(); i++) {
             JSONObject prevMove = prevMoves.getJSONObject(i);
-
+            
             int id = Integer.parseInt(prevMove.getString("id"));
             char direccion = prevMove.getString("direction").charAt(0);
+            System.out.println(id+ " " + direccion);
             switch (direccion) {
                 case 'u':
                     p[id - 1].setY(p[id - 1].getY() - 1);
@@ -207,37 +383,48 @@ public class AI {
 
         System.out.print("\n");
 
-        ArrayList<Integer> max = new ArrayList();
-        char c = 'u';
-        for (char i : directions) {
-            System.out.println(i);
-            if (isPossible(i)) {
-                switch (i)
-                {
-                    case 'u': 
-                            max.add(possibility(p[me-1].getY()-1,p[me-1].getX()));
-                             case 'd': 
-                            max.add(possibility(p[me-1].getY()+1,p[me-1].getX()));
-                             case 'r': 
-                            max.add(possibility(p[me-1].getY(),p[me-1].getX()+1));
-                             case 'l': 
-                            max.add(possibility(p[me-1].getY(),p[me-1].getX()-1));
-                }
-                int actual = possibilities(i);
-                if (actual > max) {
-                    max = actual;
-                    c = i;
-                }
+//       int[] listPoss = new int [4];
+//        for (int i = 0; i < 4; i++) {
+//            if (isPossible(directions[i])) {
+//                listPoss[i] = possibilities (directions[i]);
+//            }
+//            else listPoss[i]=-1;
+            
+            
+            
+//        }
+//        int max=0;
+//        int maxIndex= 0;
+//        
+//        for (int i = 0; i < 4; i++) {
+//            if (listPoss[i]>max) {
+//                
+//             max=   listPoss[i] ;
+//             maxIndex = i;
+//             
+//            }
+           // }
+int index=0;
+        for (int i = 0; i < 4; i++) {
+            System.out.println("dir=" + directions[i] + ", " + isPossible(directions[i]));
+            
+            if (isPossible (directions[i])) 
+                { 
+                
+                index=i;
+                
             }
-
         }
+         System.out.println(directions[index]);  
+        return directions[index];
 
         // Choisis une direction au hasard
         //  direction = directions[random.nextInt(directions.length)];
-        System.out.println("Mouvement choisi : " + direction);
+       // System.out.println("Mouvement choisi : " + direction);
+       // return directions[]//[maxIndex];
 
-        return c;
-    }
+        
+        }
 
     /**
      * Fonction appelée en fin de partie.
